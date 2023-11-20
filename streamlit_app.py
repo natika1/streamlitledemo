79779618ad5c354876f16c44989d84fd4cc30df9
natika1 <natika1@g.pl>
2023-11-20 12:25:36 +0100

    App styling - adding cool title
    st.set_page_config structure added

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,14 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(
+   page_title="Area 51",
+   page_icon="👽",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
